61503cdacb8f0e58d17c2dfc54d7677ecffaa892
DaviDio <david.cascella.5@gmail.com>
2025-02-15 13:05:36 +0100

    gitignore updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className={"app-login-box"}>
+            <h1 className={"Title"}>Sign In</h1>
+            <div>
+                <input className={"textbox"} type={"text"} placeholder={"Username"} name={"username"} />
+            </div>
+            <div>
+                <input className={"textbox"} type={"password"} placeholder={"Password"} name={"password"} />
+            </div>
+            <div>
+                <input type={"button"} className={"btn"} value={"Sign In"} />
+            </div>
+        </div>
     </div>
   );
 }
