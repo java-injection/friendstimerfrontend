8f7e58842edde3c70015ba83362582706dfbdd60
DaviDio <david.cascella.5@gmail.com>
2025-02-17 11:11:36 +0100

    login form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import './App.css';
+import account from './images/account.png'
+import lock from './images/mbrilock_99595.png'
+
 
 function App() {
   return (
     <div className="App">
         <div className={"app-login-box"}>
             <h1 className={"Title"}>Sign In</h1>
-            <div>
-                <input className={"textbox"} type={"text"} placeholder={"Username"} name={"username"} />
+            <div className={"textbox margine"}>
+                <img className={"app-icon"} src={account}/>
+                <input className={'app-input-text'} type={"text"} placeholder={"Username"} id={"name"}/>
             </div>
-            <div>
-                <input className={"textbox"} type={"password"} placeholder={"Password"} name={"password"} />
+            <div className={"textbox"}>
+                <img className={"app-icon"} src={lock}/>
+                <input className={'app-input-text'} type={"password"} placeholder={"Password"} id={"password"}/>
             </div>
-            <div>
-                <input type={"button"} className={"btn"} value={"Sign In"} />
+            <div className={"register"}>
+
+                forgot password?
             </div>
+            <button onClick={login} className={"login-button"}>
+                Sign In
+            </button>
+
+            <div className={"register"} onClick={goToRegister}>or Register</div>
+
         </div>
     </div>
   );
+}
+function goToRegister(){
+    console.log("ciao")
+    window.location.href = "https://google.com";
+}
+async function login(){
+    var password = document.getElementById('password').value;
+    var username = document.getElementById('name').value;
+
 }
 
 export default App;
